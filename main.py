12d53453da5f384e7e2758acd8598056472cfa8a
Edmond Cane <76094476+ecane2000@users.noreply.github.com>
2023-12-03 12:46:22 +0800

    add files via upload
    this is just to start

---
+++ main.py
@@ -0,0 +1,11 @@
+import spacy
+
+def main():
+    # Load spaCy language model
+    nlp = spacy.load("en_core_web_sm")
+
+    # Your corpus processing logic here
+    # ...
+
+if __name__ == "__main__":
+    main()
